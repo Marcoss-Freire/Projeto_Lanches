b529b105ebe9af2f1ea473cede7480d53fb78849
Thurtux <arthursantoslopes91@gmail.com>
2024-05-16 08:52:44 -0300

    Populando a tabela Lanche

---
+++ Projeto3DS/Migrations/20240514164106_PopCategoria.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Mysqlx.Crud;
+using Projeto3DS.Models;
+
+#nullable disable
+
+namespace Projeto3DS.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert Into Categoria(CategoriaNome, descricao) values ('normal','lanches feitpo com ingredientes normais')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categoria");
+        }
+    }
+}
---
+++ Projeto3DS/Migrations/20240514170228_Lanches.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto3DS.Migrations
+{
+    /// <inheritdoc />
+    public partial class Lanches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert Into Lanche(descricao_curta,descricao_detalhada,emEstoque,lancheId,nome,preco,imagemURL,lanchePreferido) values ('Lanche muito gostoso','Lanche feito com as melhores carnes do mercados com alface e tomate frescos',1,1,'555Deli',50.00,'ceily.jpg','Sim')");
+            migrationBuilder.Sql("Insert Into Lanche(descricao_curta,descricao_detalhada,emEstoque,lancheId,nome,preco,imagemURL,lanchePreferido) values ('Lanche muito suculento','Lanche feito com os melhores alface e tomate',1,2,'100Condi',60.00, 'max.jpg','Nao')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Lanche");
+        }
+    }
+}
